b3d5b643748b256d828f5ecd60f4e84b98b8660b
YisusMS <jesus.antonio7garcia@gmail.com>
2021-09-09 11:13:04 -0500

    Primer commit

---
+++ src/main/java/com/msa/myapp/entity/Test.java
@@ -0,0 +1,20 @@
+package com.msa.myapp.entity;
+
+/**
+ * @author megasum
+ *
+ */
+public class Test {
+
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
